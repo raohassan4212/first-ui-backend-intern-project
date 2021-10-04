568956002a6a6f69bd450aaaac7ac435d8e3a3dc
raohassan4212 <52700173+raohassan4212@users.noreply.github.com>
2021-10-04 13:52:37 +0500

    branch forget code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 // app.use(express.urlencoded({extended:false}));
 var cors = require('cors');
-var { addUser, getUsers } = require('./mock/users');
+var { addUser, getUsers, getVerifiedEmail } = require('./mock/users');
 var { sendVerificationCode } = require('./services/email-service');
 var { verifyCode, addTempUser, getTempUser, randomCode, addCode } = require('./services/verification-service');
 var dotenv = require('dotenv');
@@ -14,8 +14,16 @@ app.use(cors());
 
 app.use(express.json());
 
+// let Users = [
+//     {id: "1",  firstname: "hassan", lastname: "hassan", number:"789456", email: "raohassan4212@gmail.com", confirmemail: "", password: "123456"},
+//     {id: "2",  firstname: "Ibrahim", lastname: "last",number:"789456", email: "ibrahimn@gmail.com", confirmemail: "", password: "123"},
+//     {id: "3",  firstname: "Ali", lastname: "last",number:"789456", email: "ali@gmail.com", confirmemail: "", password: "123"},
+//     {id: "4",  firstname: "Khan", lastname: "last",number:"789456", email: "khan@gmail.com", confirmemail: "", password: "123"},
+// ]
+
 app.get("/", (req, res) => {
     var userr = getUsers()
+    console.log("get User", userr);
     res.send(`Users: <br> ${JSON.stringify(userr)}`)
 })
 
@@ -46,26 +54,24 @@ app.post("/create", async (req, res) => {
 
 app.post("/verify", async (req, res) => {
     var { email, code } = req.headers;
+    console.log(email);
+    console.log(code);
     if (verifyCode(email, code)) {
         var user = getTempUser(email);
         await addUser(user);
+        console.log(user);
         res.send("Ok")
+        // res.sendStatus(200);
     } else {
         res.sendStatus(400);
+        console.log("Some err")
     }
 })
 
 app.post("/forget", (req, res) => {
-    var { email } = req.headers;
-    var signUpUser = getUsers()
-    var forgetEmailVerification = signUpUser.find((user) => {
-        if (user.email === email) {
-            var code = randomCode();
-            addCode(email, code);
-        }
-    });
-    res.send(forgetEmailVerification);
-
+    
+    console.log(signUpUser);
+    res.send("if ky bahar  wala scope")
 })
 
 app.listen("5000", () => {
--- mock/users.js
+++ mock/users.js
@@ -1,11 +1,21 @@
 const Users = [ 
-    {id: "1",  firstname: "Hassan", lastname: "lastname", number:"789456", email: "hassan@gmail.com", confirmemail: "", password: "123"},
+    {id: "1",  firstname: "Hassan", lastname: "lastname", number:"789456", email: "raohassan4212@gmail.com", confirmemail: "", password: "123"},
     {id: "2",  firstname: "Ibrahim", lastname: "lastname", number:"789456", email: "ibrahimn@gmail.com", confirmemail: "", password: "123"},
     {id: "3",  firstname: "Ali", lastname: "lastname", number:"789456", email: "ali@gmail.com", confirmemail: "", password: "123"},
     {id: "4",  firstname: "Khan", lastname: "lastname", number:"789456", email: "khan@gmail.com", confirmemail: "", password: "123"},
 ];
 
 module.exports.getUsers =  () => { return [...Users]};
+module.exports.getVerifiedEmail = () => {
+    return   signUpUser = users.find( async (users) => {
+        if (users.email === email) {
+            var code = randomCode();
+            addCode(email, code);
+            await sendVerificationCode(email, code);
+            res.send("if wala scope")
+        }
+    });
+}
 
 module.exports.addUser = async (user) => {
     user = {id: `${Users.length + 1}`, ...user}
